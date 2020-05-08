3a264417a1cd70ee253bca3bf0fb18f6d37e1a7b
hyy <1005436852@qq.com>
2020-05-08 14:36:30 +0800

    first commit

---
+++ demo.py
@@ -0,0 +1,12 @@
+from python.demo1 import Demo1
+# from testing import demo2
+# from testing.demo2 import *
+# from testing.demo2 import Demo2
+
+# Demo1()
+# print(hello)
+# print(demo2.hello)
+# demo2.f()
+
+from testing import *
+
---
+++ testing/demo2.py
@@ -0,0 +1,10 @@
+
+__all__ = ['hello']
+
+hello = 'hello demo2'
+
+
+def f():
+    print("demo2 f()")
+class Demo2:
+    pass
