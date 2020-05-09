b44f7af22f4b8762893f6035e478f5f9a03aa72d
hyy <1005436852@qq.com>
2020-05-09 16:25:51 +0800

    commit 计算机加法、除法测试

---
+++ testing/test_calc.py
@@ -0,0 +1,14 @@
+import unittest
+from python.calc import Calc
+
+
+class TestCalc(unittest.TestCase):
+    def test_add(self):
+        self.calc = Calc()
+        result = self.calc.add(1, 2)
+        print(result)
+        self.assertEqual(3, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ testing/test_pytest_cacl.py
@@ -0,0 +1,31 @@
+import pytest
+import allure
+from python.calc import Calc
+
+
+class TestCalc:
+    @pytest.fixture()
+    @allure.step('实例化Calc')
+    def setup(self):
+        self.calc = Calc()
+
+    @allure.story('加法测试')
+    @pytest.mark.parametrize('a, b, result', [(1, 2, 3), (-1, -2, -3), (0, 0, 0), (0.15, 0.01, 0.16)])
+    def test_add_1(self, setup, a, b, result):
+        sum = self.calc.add(a, b)
+        print(result)
+        assert sum == result
+
+    @allure.story('除法测试')
+    @pytest.mark.parametrize('a, b, result', [(1, 1, 1), (-2, -2, 1), (0.1, 0.2, 0.5), (0, 0, 0)])
+    def test_div(self, setup, a, b, result):
+        try:
+            quotient = self.calc.div(a, b)
+            print(quotient)
+            assert quotient == result
+        except ZeroDivisionError as e:
+            print(e.message)
+
+
+if __name__ == "__main__":
+    pytest.main(['-vs', 'test_pytest_cacl.py::TestCalc'])
